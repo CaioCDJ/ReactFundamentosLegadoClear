e5f1773a25c82003a09141588b863b4f6528ca2a
CaioCDJ <caiodjesus1@hotmail.com>
2022-07-04 16:57:45 -0300

    this Problem in Js

--- src/index.js
+++ src/index.js
@@ -4,21 +4,35 @@ import React from 'react';
 //import PrimeiroComponente from './componentes/primeiroComponente.jsx';
 //import {CompA,CompB} from './componentes/DoisComponentes.jsx';
 //import Multi from './componentes/MultiElementos.jsx'
-import FamiliaSilva from './componentes/FamiliaSilva.jsx';
-import Familia from './componentes/Familia';
-import Membro from './componentes/Membro';
+//import FamiliaSilva from './componentes/FamiliaSilva.jsx';
+//import Familia from './componentes/Familia';
+//import Membro from './componentes/Membro';
+//import ComponenteComFuncao from './componentes/ComponenteComFuncao';
+//import Pai from './componentes/Pai.jsx';
+//import Classe from './componentes/ComponenteClasse.jsx';
 
+import Contador from './componentes/Contador';
 
 const elemento = document.getElementById('root');
 
 ReactDOM.render(
   <>
+    <Contador numero={0} />
+    {/*
+    <Classe valor="Sou uma classe"/>
+   
+    <Pai />
+   
+    <ComponenteComFuncao />
+    
     <Familia sobrenome="Silva">
       <Membro nome="Andre"  />
       <Membro nome="Mariana" />
     </Familia>
     <br/>
     <FamiliaSilva sobrenome="Silva Pereira"/>
+  */}
+
   </>
   ,elemento)
 
